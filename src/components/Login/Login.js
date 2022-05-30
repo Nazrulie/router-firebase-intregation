75c925378f954e4d106846159abd7ed3298f5459
Nazrulie <emon.bbc7@gmail.com>
2022-05-30 22:29:23 +0600

    finish

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import useFirebase from '../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { getAuth } from 'firebase/auth';
+import app from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
+const auth = getAuth(app)
 
 const Login = () => {
-    const { singInWithGoogle } = useFirebase();
+    const [signInwithGoogle] = useSignInWithGoogle(auth)
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInwithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
     return (
         <div>
-            <h3>please login</h3>
+            <h1>please login</h1>
             <div style={{ margin: '10px' }}>
-                <button onClick={singInWithGoogle}>Google sign in</button>
+                <button onClick={handleGoogleSignIn}>Google sign in</button>
             </div>
-            <br />
-            <form >
-                <input type="email" placeholder='Your email' />
+            <form>
+                <input type="email" placeholder='Your Email' />
                 <br />
-                <input type="password" name="" id="" placeholder='password' />
+                <input type="password" name="" id="" placeholder='Password' />
                 <br />
-                <input type="submit" value="Login" />
+                <input type="submit" value="login" />
             </form>
         </div>
     );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,18 +2,17 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
-import useFirebase from '../hooks/useFirebase';
 
 const auth = getAuth(app)
 
-const Home = () => {
+const Products = () => {
     const [user] = useAuthState(auth)
     return (
         <div>
-            <h2>this is home</h2>
-            <p>Current user :{user ? user.displayName : 'No body'}</p>
+            <h2>Knock knock ! Who is ther </h2>
+            <h4>{user ? user.displayName : 'bhoot'}</h4>
         </div>
     );
 };
 
-export default Home;
+export default Products;
